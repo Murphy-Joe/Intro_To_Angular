bd6ff1b8f889f549c0b21069ff86ea40a66e40e6
Jeffry Gonzalez <jeff@hypertheory.com>
2021-11-15 16:31:20 -0500

    done with monday

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerStatusComponent } from './components/server-status/server-status.component';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ResourcesComponent } from './components/resources/resources.component';
+import { NavComponent } from './components/nav/nav.component'
 @NgModule({
   declarations: [
     AppComponent,
-    ServerStatusComponent
+    ServerStatusComponent,
+    DashboardComponent,
+    ResourcesComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
