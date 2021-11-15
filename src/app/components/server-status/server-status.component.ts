7d9f08aae967e94104169f4c2559fc1c88613417
Jeffry Gonzalez <jeff@hypertheory.com>
2021-11-15 16:07:49 -0500

    ready to push this thing

---
+++ src/app/components/server-status/server-status.component.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { StatusResponseModel } from 'src/models/statusresponse.model';
+import { environment } from '../../../environments/environment';
+@Component({
+  selector: 'app-server-status',
+  templateUrl: './server-status.component.html',
+  styleUrls: ['./server-status.component.css']
+})
+export class ServerStatusComponent implements OnInit {
+
+  model: StatusResponseModel = {
+    message: 'Fake Server Message',
+    lastChecked: '2021-11-15T20:15:41.640Z'
+  };
+
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get<StatusResponseModel>(environment.apiUrl + 'status').subscribe(r => {
+      this.model = r;
+    });
+  }
+
+}
---
+++ src/models/statusresponse.model.ts
@@ -0,0 +1,5 @@
+
+export interface StatusResponseModel {
+  message: string;
+  lastChecked: string;
+}
